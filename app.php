6819cbc3a266b81c838c617100238bf41d66b8f6
FAYA1702 <fayemor1702@gmail.com>
2025-06-02 13:33:51 -0400

    delete
    supprimer les fichier

--- app.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Afficher un message apres 3 secondes 
-sleep(3);
-echo "Bonjour Teccart, après un délai de 3 secondes";
-?>
--- app.py
+++
@@ -1,6 +0,0 @@
-import time
-
-# Afficher les 10 premiers chiffres avec 1 seconde entre chaque
-for i in range(1, 11):
-    print(i)
-    time.sleep(1)  # Pause de 1 seconde
